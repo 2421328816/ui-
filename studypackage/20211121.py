ccd80c7459328763fbf5d9413ba25fbbf88dbc92
2421328816@qq.com <242132816@qq.com>
2021-11-21 21:05:11 +0800

    20211121学习的代码

---
+++ studypackage/20211121.py
@@ -0,0 +1,10 @@
+#-*-  coding:utf-8  -*-
+#@File:20211121.py
+#@Time:2021/11/2111:44
+#@site:
+#@Software:PyCharm
+#@Author:sfz
+#
+str1='   dfd   fds   '
+str2=str1.strip().replace(' ','').split('df')
+print(str2)
